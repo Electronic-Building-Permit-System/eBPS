be0f3b27b4f20e42fdaf96bb9680a3a877f437c3
pukarsai <85217415+pukarsai@users.noreply.github.com>
2024-12-06 21:53:19 +0545

    Clean Architecture

--- eBPS.Server/Program.cs
+++ eBPS.Server/Program.cs
@@ -1,12 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigins", policy =>
+    {
+        policy.WithOrigins("*")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors("AllowSpecificOrigins");
 
 app.UseHttpsRedirection();
 
