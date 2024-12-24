f9cc30d119079aa43aa59d92cbeae4e95f940734
pukarsai <85217415+pukarsai@users.noreply.github.com>
2024-12-24 23:05:04 +0545

    Removing Signup and login for test

--- eBPS.Client/src/app/app.routes.ts
+++ eBPS.Client/src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { LoginComponent } from './account/login/login.component';
-import { SignupComponent } from './account/signup/signup.component';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FooterComponent } from './shared/footer/footer.component';
@@ -10,11 +8,10 @@ import { HomeNavbarComponent } from './shared/home-navbar/home-navbar.component'
 export const routes: Routes = [
   {
     path: '',
-    component: LoginComponent
+    component: HomeComponent
   },
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
-  { path: 'signup', component: SignupComponent },
   { path: 'navbar', component: HomeNavbarComponent },
   { path: 'footer', component: FooterComponent },
   { path: 'dashboard', component: DashboardComponent },
