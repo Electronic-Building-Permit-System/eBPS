ef1a60f3a157844fb2efa7f2486db210b01f4e94
pukarsai <85217415+pukarsai@users.noreply.github.com>
2024-12-31 23:41:57 +0545

    Update applicant-details.component.spec.ts

--- eBPS.Client/src/app/dashboard/createapplication/applicant-details/applicant-details.component.spec.ts
+++ eBPS.Client/src/app/dashboard/createapplication/applicant-details/applicant-details.component.spec.ts
@@ -3,14 +3,17 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ApplicantDetailsComponent } from './applicant-details.component';
 import { ChangeDetectorRef, ElementRef } from '@angular/core';
 import { CdkStepper } from '@angular/cdk/stepper';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ApplicantDetailsComponent', () => {
   let component: ApplicantDetailsComponent;
   let fixture: ComponentFixture<ApplicantDetailsComponent>;
+  let secondFormGroup: FormGroup;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ApplicantDetailsComponent],
+      imports: [ApplicantDetailsComponent, BrowserAnimationsModule],
       providers: [
         ChangeDetectorRef,
         { provide: ElementRef, useValue: {} }, 
@@ -21,6 +24,19 @@ describe('ApplicantDetailsComponent', () => {
 
     fixture = TestBed.createComponent(ApplicantDetailsComponent);
     component = fixture.componentInstance;
+
+    // Create a FormGroup for the @Input() secondFormGroup
+    const fb = new FormBuilder();
+    secondFormGroup = fb.group({
+      salutation: [''],
+      applicantName: [''],
+      phone: [''],
+      email: [''],
+      wardNo: [''],
+      address: [''],
+      houseNo: [''],
+    });
+    component.secondFormGroup = secondFormGroup;
     fixture.detectChanges();
   });
 
