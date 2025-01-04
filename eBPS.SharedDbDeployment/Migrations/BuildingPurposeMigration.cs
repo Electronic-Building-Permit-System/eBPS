725b2e7daf75d460b8b336713d555b997154de82
pukarsai <85217415+pukarsai@users.noreply.github.com>
2025-01-04 18:26:42 +0545

    Migration using bat file

---
+++ eBPS.SharedDbDeployment/Migrations/BuildingPurposeMigration.cs
@@ -0,0 +1,53 @@
+﻿using FluentMigrator;
+
+namespace eBPS.SharedDbDeployment.Migrations
+{
+    [Migration(202501021047)]
+    public class CreateBuildingPurposeTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("BuildingPurpose")
+            .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+            .WithColumn("Description").AsString(100).NotNullable()
+            .WithColumn("IsActive").AsBoolean().NotNullable().WithDefaultValue(true);
+
+            // Insert initial data
+
+            Insert.IntoTable("BuildingPurpose").Row(new
+            {
+                Description = "Residential",
+            });
+            Insert.IntoTable("BuildingPurpose").Row(new
+            {
+                Description = "Commercial",
+            });
+            Insert.IntoTable("BuildingPurpose").Row(new
+            {
+                Description = "Residential Cum Commercial",
+            });
+            Insert.IntoTable("BuildingPurpose").Row(new
+            {
+                Description = "Industrial",
+            });
+            Insert.IntoTable("BuildingPurpose").Row(new
+            {
+                Description = "Government",
+            });
+            Insert.IntoTable("BuildingPurpose").Row(new
+            {
+                Description = "Education",
+            });
+            Insert.IntoTable("BuildingPurpose").Row(new
+            {
+                Description = "Health",
+            });
+        }
+
+
+        public override void Down()
+        {
+            Delete.Table("BuildingPurpose");
+        }
+    }
+}
