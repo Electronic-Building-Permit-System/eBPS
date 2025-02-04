0942729af37b0a408e3faf82ef9b3d6511b822bd
Sangeet Neupane <s2058n@gmail.com>
2025-02-04 22:16:43 +0545

    table

--- eBPS.Client/src/app/components/dashboard/designdata/floor/floor.component.ts
+++ eBPS.Client/src/app/components/dashboard/designdata/floor/floor.component.ts
@@ -12,25 +12,28 @@ import { MatStepperModule } from '@angular/material/stepper';
   selector: 'app-floor',
   standalone: true,
   imports: [CommonModule,
-        ReactiveFormsModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatButtonModule,
-      MatButtonModule,MatOptionModule,MatSelectModule,MatStepperModule],
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatButtonModule, MatOptionModule, MatSelectModule, MatStepperModule],
   templateUrl: './floor.component.html',
   styleUrls: ['./floor.component.css']
 })
 export class FloorComponent implements OnInit {
-  
-  predefinedFloors: string[] = ['Boundary Wall','Basement 1','Basement 2',
+
+  predefinedFloors: string[] = [
+    'Boundary Wall', 'Basement 1', 'Basement 2',
     'Semi Basement',
     'Ground Floor',
     'First Floor',
     'Second Floor',
     'Third Floor',
-    'Fourth Floor'
+    'Fourth Floor',
+    'Fifth Floor',
+    'Sixth Floor'
   ];
- 
+
   @Input() floorForm!: FormArray;
 
   constructor(private fb: FormBuilder) {
@@ -45,17 +48,16 @@ export class FloorComponent implements OnInit {
   }
 
   initializeFloors(): void {
-    this.predefinedFloors.forEach((floorName) => {
-      this.floorForm.push(this.createFloorFormGroup(floorName));
-    });
+    // Initialize with the first 4 floors
+    for (let i = 0; i < 4; i++) {
+      this.floorForm.push(this.createFloorFormGroup(this.predefinedFloors[i]));
+    }
   }
-
 
   getFormGroup(control: any): FormGroup {
     return control as FormGroup;
   }
 
-  
   // Create a new FormGroup for a floor
   createFloorFormGroup(floorName: string): FormGroup {
     return this.fb.group({
@@ -71,7 +73,16 @@ export class FloorComponent implements OnInit {
     });
   }
 
-  
+  addFloor(): void {
+    const nextFloorIndex = this.floorForm.length;
+    if (nextFloorIndex < this.predefinedFloors.length) {
+      this.floorForm.push(this.createFloorFormGroup(this.predefinedFloors[nextFloorIndex]));
+    }
+  }
+
+  removeFloor(): void {
+    if (this.floorForm.length > 4) {
+      this.floorForm.removeAt(this.floorForm.length - 1);
+    }
+  }
 }
-
-
